78ce1ef335279b3b144d82c134ce531137f798b2
choilettuce <tkdgnstkdcn@naver.com>
2022-07-22 17:29:16 +0900

    study: nodejs

---
+++ nodejs/220722/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.set('view engine', 'ejs');
+app.use( '/abc', express.static('public')); // '/abc'가상경로 설정
+
+app.get('/', (req,res) => {
+    res.render("0722-1", {a: 'aaa', b:'bbb'}); // render 이용시에는 확장자 안적어도됨
+});
+
+app.listen(port, () => {
+    console.log('server port : ', port);
+});
+
