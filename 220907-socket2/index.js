d04eeebeacc4a7cdbe85fcb0bcc992c30363be5b
choilettuce <tkdgnstkdcn@naver.com>
2022-09-14 17:10:57 +0900

    220914 socket

--- 220907-socket2/index.js
+++ 220907-socket2/index.js
@@ -5,24 +5,29 @@ var io = require("socket.io")(http);
 
 app.get("/", function(req,res){
     console.log("client");
-    res.sendFile(__dirname + "/index.html");
-
+    res.sendFile( __dirname + "/index.html" );
 });
+
+var msg = {hello: "안녕하세요?", study: "공부합시다~"};
 io.on("connection", function(socket){
-    console.log("connected");
-    socket.emit("hello", "안녕");
-    
+    console.log( "connected" );
+    socket.on("send", function(data){
+        // data로 hello가 들어왔을 때
+        console.log( "client : ", data );
+        socket.emit("response", data + " : " + msg[data]);
+        // socket.emit( "response", "hello : 안녕하세요?");
+        // if ( data == "hello" ) socket.emit("response", "hello : 안녕하세요?");
     })
     socket.on("hello", function(data){
-        socket.emit("Response", "안녕하세요");
-    
+        console.log( "client : ", data );
+        socket.emit("response", "hello : 안녕하세요?");
     });
-
     socket.on("study", function(data){
-        socket.emit("Response", "공부합시다");
-    
-    });
+        console.log( "client : ", data );
+        socket.emit("response", "study : 공부합시다~");
+    })
+});
 
-http.listen(8000, function(){
-    console.log("Server port :", 8000);
+http.listen( 8000, function(){
+    console.log( "Server port : ", 8000 );
 });
