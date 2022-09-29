8d7b9e8522534bf441698f7ff8e553d2585a1ca2
choilettuce <tkdgnstkdcn@naver.com>
2022-09-30 05:25:01 +0900

    정리

---
+++ html&CSS기초/220905_css실습/problem.js
@@ -0,0 +1,9 @@
+// js 동작 채우기
+
+const icon_menu = document.querySelector('.icon_menu');
+const slide_menu = document.querySelector('.slide_menu');
+
+icon_menu.addEventListener('click', function() {
+    slide_menu.classList.toggle('d-none')
+});
+// 토글 추가했다가 
