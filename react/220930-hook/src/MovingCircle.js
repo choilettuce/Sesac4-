736e1b4b1ec29ad6e98f966853f67eb715bc0c36
choilettuce <tkdgnstkdcn@naver.com>
2022-10-06 21:52:03 +0900

    221006 airbnb_clone

---
+++ react/220930-hook/src/MovingCircle.js
@@ -0,0 +1,13 @@
+import './MovingCircle.scss';
+
+const MovingCircle = () => {
+    return (
+<div className='MovingCircle'>
+    <div className='Circle Pink'></div>
+    <div className='Circle Yellow'></div>
+    <div className='Circle Green'></div>
+    </div>
+    )
+} 
+
+export default MovingCircle;
---
+++ react/airbnb-clone/src/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className="Banner">
+        <h1>I am Banner</h1>
+    </div>
+  );
+}
+
+export default Banner;
