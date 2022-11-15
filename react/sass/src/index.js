516af3f8e7f3347395ef9e4e5ed8cf1475bb0a2e
choilettuce <tkdgnstkdcn@naver.com>
2022-11-15 18:05:03 +0900

    폴더정리

---
+++ react/sass/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import Worm from './Worm';
+import UseSass from './5UseSass';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <UseSass/>
+  </React.StrictMode>
+);
+
